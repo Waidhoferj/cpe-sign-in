7f031ef89143528a3ab613618594d9ed5cbe74d6
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-14 21:23:48 -0800

    customized link resolver

--- src/link-resolver.js
+++ src/link-resolver.js
@@ -1,12 +1,7 @@
 export default function(doc) {
   // Return the path depending on Prismic Document's type
-  // If it is a Single Custom Type with the API ID of "home"
-  if (doc.type === "home") {
-    return "/";
-  }
-  // If it is a Repeatable Custom Type with the API ID of "page" (must contain a UID field)
-  if (doc.type === "page") {
-    return "/page/" + doc.uid;
+  if (doc.type === "overview") {
+    return "/brief";
   }
 
   // Default to the root
