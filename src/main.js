f711def2e5bb08fcb16955381d6c1bac04ba2c4b
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-22 23:48:35 -0800

    added prismic entry point outside of index.html

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Vuelidate from "vuelidate";
+window.prismic = {
+  endpoint: "https://cpe-sign-in.cdn.prismic.io/api/v2"
+};
 import PrismicVue from "prismic-vue";
 import linkResolver from "./link-resolver";
 import VueWaypoint from "vue-waypoint";
