4bde0a97c6ca956ad9aaa88a6026cc567d575fe4
Waidhoferj <jwaidhof@calpoly.edu>
2020-01-23 00:03:14 -0800

    set default path

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,12 @@ const routes = [
     props: true,
     component: () => import("../views/Events.vue")
   }
+  {
+    path: "*",
+    name: "default",
+    props: true,
+    component: SignIn
+  }
 ];
 
 const router = new VueRouter({
